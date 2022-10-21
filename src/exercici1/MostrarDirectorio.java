64cc821b7e7bb2d55f7f206e848f71e119dbda09
Arbol23 <karinantunez22@gmail.com>
2022-10-21 18:20:17 +0200

    mi primer commit

---
+++ src/exercici1/MostrarDirectorio.java
@@ -0,0 +1,19 @@
+package exercici1;
+
+
+	import java.io.File;
+	import java.util.Arrays;
+
+	public class MostrarDirectorio {
+
+	    public static void main(String[] args) {
+	        File directorio = new File("c:/"); //directorio a listar                                             
+	        String[] lista = directorio.list();
+	        Arrays.sort(lista);
+	        for (int i = 0; i < lista.length; i++) {
+	            System.out.println(lista[i]);
+	        }
+	    }
+	}
+
+
